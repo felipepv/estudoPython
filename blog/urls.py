60596a17d754ff4d99d7bdded44dd83e99bb55ca
Felipe Vieira <lipe.vieira@hotmail.com>
2020-04-28 20:33:45 -0300

    Primeira versão estudo

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+path('', views.home, name="home"),
+path('post/<int:pk>/', views.detalhe_post, name="detalhe_post"),
+path('editar_postagem/<int:pk>/', views.edit_postagem, name="edicao_postagem"),
+path('post/new/', views.adicionar_postagem, name="adicionar_postagem")
+]
+
---
+++ blog/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from blog.models import Postagem
+from .forms import PostForm
+from django.utils import timezone
+from django.shortcuts import redirect,get_object_or_404
+# Create your views here.
+def home(request):
+	postagens = Postagem.objects.all().order_by('-data_criacao')
+	return render(request, 'home.html', {'postagens':postagens})
+
+def detalhe_post(request, pk):
+	postagem = Postagem.objects.get(pk=pk)
+	return render(request, 'detalhe_post.html', {'postagem':postagem})
+
+def adicionar_postagem (request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		postagem = form.save(commit=False)
+		postagem.autor = request.user
+		postagem.data_publicacao = timezone.now()
+		postagem.save()
+		return redirect ('home')
+	else:
+		form = PostForm()
+	return render(request, 'editar_postagem.html', {'form':form})
+
+def edit_postagem(request, pk):
+	postagem = get_object_or_404(Postagem, pk=pk)
+	if request.method == "POST":
+		form = PostForm(request.POST, instance=postagem)
+		if form.is_valid():
+			# Quando for para salvar
+			postagem = form.save(commit=False)
+			postagem.autor = request.user
+			postagem.data_publicacao = timezone.now()
+			postagem.save()
+			return redirect('home')
+
+	else:
+		# Primeiro Acesso
+		form = PostForm(instance=postagem)
+
+	return render(request, 'editar_postagem.html', {'form':form})
